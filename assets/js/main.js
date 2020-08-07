26f564d5c2406b6bcd497169a2c947fda596afa9
anhkhoa <leanhkhoa30101998@gmail.com>
2020-08-07 17:32:24 +0700

    abc

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+{/* <script type="text/javascript">
+    $('.autoplay').slick({
+        slidesToShow: 5,
+        slidesToScroll: 2,
+        autoplay: true,
+        autoplaySpeed: 1500,
+    });
+    $('li.header__navbar-top-item.header__navbar-top-item--right').click(function(){
+        $("ul.header__navbar-top").addClass( "d-none")
+    });
+</script> */}
